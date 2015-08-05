03f18c908530990bc01e554a8714dfca446d8077
adam <adam@reddit.com>
2015-08-05 11:11:33 -0700

    Revert "Move loid to globals"
    This reverts commit bc3cb03bd5fe21cedb6037e6ccc4d5b0dd6f6ee4.
    
    Conflicts:
    	src/globals.es6.js
    	src/routes.jsx
    	src/views/pages/index.jsx
    	src/views/pages/messages.jsx
    	src/views/pages/search.jsx
    	src/views/pages/subredditAbout.jsx
    	src/views/pages/userActivity.jsx
    	src/views/pages/userGild.jsx
    	src/views/pages/userProfile.jsx
    	src/views/pages/userSaved.jsx

--- src/views/pages/userGild.jsx
+++ src/views/pages/userGild.jsx
@@ -58,6 +58,8 @@ class UserGildPage extends BaseComponent {
         <TrackingPixel
           url={ this.state.data.meta.tracking }
           user={ this.props.user }
+          loid={ props.loid }
+          loidcreated={ props.loidcreated }
         />);
     }
 
