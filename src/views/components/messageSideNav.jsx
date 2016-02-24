543fbabfb2bf1da5a0444cf04f68bce496ccc461
curioussavage <curioussavage01@gmail.com>
2016-02-23 19:57:30 -0800

    desktop messages prototype

--- src/views/components/messageSideNav.jsx
+++ src/views/components/messageSideNav.jsx
@@ -2,15 +2,49 @@ import React from 'react';
 
 import BaseComponent from './BaseComponent';
 
+const BASE_MENU = [
+  { link: 'all'},
+  { link: 'unread'},
+];
+
+const USER_MENU = [
+  { link: 'messages' },
+  { link: 'comments', title: 'comment replies' },
+  { link: 'selfreply', title: 'post replies'},
+  { link: 'mentions', title: 'username mentions' },
+];
+
+const INBOX_MENU = BASE_MENU.concat(USER_MENU); 
+
 class MessageSideNav extends BaseComponent {
   static propTypes = {
 
   };
 
+  renderMenuItem(sub) {
+    const subPath = sub ? `/${sub}` : '';
+    return (curr, index) => {
+      const path = curr.link === 'all' ? '' : curr.link;
+      const link = `${subPath}/messages/${curr.link}`;
+      return (
+        <li>
+          <a href={ link } data-no-route='true'>{ curr.text || curr.link } </a>
+        </li>
+      );
+    }
+  }
+
   render() {
+    const props = this.props;
+
+    const items = props.inboxType === 'moderator' ? 
+      BASE_MENU.map(this.renderMenuItem(props.subreddit)) : INBOX_MENU.map(this.renderMenuItem(props.subreddit));
+
     return (
-      <div>
-        hello world
+      <div className='messageSidenav'>
+        <ul>
+          { items }
+        </ul>
       </div>
     );
   }
